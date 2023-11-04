a5c93c850d934c86e1c3069f0f8f545e05be5d43
AndronakyAndrew <andronakya@gmail.com>
2023-11-03 23:09:02 -0400

    Add project files.

---
+++ 3 попытки на аутентификацию/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Добро пожаловать в программу!");
+        Console.WriteLine("Введите логин:");
+        string login = Console.ReadLine();
+        Console.WriteLine("Введите пароль:");
+        string password = Console.ReadLine();
+
+        int attempt = 0;
+
+        while (attempt < 3)
+        {
+            if (login == "admin" && password == "12345")
+            {
+                Console.WriteLine("Вы успешно авторизовались!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели неверный логин или пароль!");
+                attempt++;
+                Console.WriteLine($"Осталось {3 - attempt} попыток!");
+                Console.WriteLine("Введите логин:");
+                login = Console.ReadLine();
+                Console.WriteLine("Введите пароль:");
+                password = Console.ReadLine();
+            }
+        }
+    }
+}
